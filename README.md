8b91f17bc3c0fe2b6585fa9ce1636cc4df8a9f17
Jonathan David Rodriguez Lozada <102917544+jonathandrl24@users.noreply.github.com>
2024-10-26 10:32:10 -0500

    antes de grafica dashboard

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # PAYMASTER-SPRING
 
+Aplicación web spring boot Java maven- GESTION DE PAGOS EMPRESA DE CONSTRUCCION - Proyecto
+
 
--- src/main/java/com/paymaster/controller/HomeController.java
+++ src/main/java/com/paymaster/controller/HomeController.java
@@ -158,7 +158,7 @@ public class HomeController {
 
 
 	// cancelar pago paypal
-	@GetMapping(value = CANCEL_URL)
+	@GetMapping("/payment/cancel")
 	public String cancelPay() {
 		return "usuario/cancel";
 	}
