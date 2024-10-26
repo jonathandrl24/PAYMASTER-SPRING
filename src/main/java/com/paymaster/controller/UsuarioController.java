c529569dcd3da57605043a93e50a8f8656df5b6d
Jonathan David Rodriguez Lozada <102917544+jonathandrl24@users.noreply.github.com>
2024-10-26 03:01:50 -0500

    Punto de control
    Todo lo hecho parece funcionar en orden

--- src/main/java/com/paymaster/controller/UsuarioController.java
+++ src/main/java/com/paymaster/controller/UsuarioController.java
@@ -60,25 +60,24 @@ public class UsuarioController {
 	public String acceder(Usuario usuario, HttpSession session) {
 		logger.info("Accesos : {}", usuario);
 
-		// Verifica que el usuario existe en la base de datos
-		Optional<Usuario> user = usuarioService.findByEmailAndPassword(usuario.getEmail(), usuario.getPassword());
-		// Aquí, asegúrate de que estás buscando por email y password o por lo que sea necesario para la autenticación
+		Optional<Usuario> user=usuarioService.findById(Integer.parseInt(session.getAttribute("idusuario").toString()));
+		//logger.info("Usuario de db: {}", user.get());
 
 		if (user.isPresent()) {
-			// Si el usuario existe, guardamos su ID en la sesión
 			session.setAttribute("idusuario", user.get().getId());
 
 			if (user.get().getTipo().equals("ADMIN")) {
 				return "redirect:/administrador";
-			} else {
+			}else {
 				return "redirect:/";
 			}
-		} else {
+		}else {
 			logger.info("Usuario no existe");
 		}
 
 		return "redirect:/";
 	}
+
 
 
 	@GetMapping("/compras")
